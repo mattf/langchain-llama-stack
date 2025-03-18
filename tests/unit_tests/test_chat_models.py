6b403b79c2664619dec87a1bdd05726b36972966
Matthew Farrellee <matt@cs.wisc.edu>
2025-03-18 11:09:16 -0400

    add serialization support

--- tests/unit_tests/test_chat_models.py
+++ tests/unit_tests/test_chat_models.py
@@ -1,4 +1,4 @@
-from typing import Type
+from typing import Tuple, Type
 
 from langchain_tests.unit_tests import ChatModelUnitTests
 
@@ -15,6 +15,26 @@ class TestChatLlamaStackUnit(ChatModelUnitTests):
         return {
             "model": "not-a-model",
         }
+
+    @property
+    def init_from_env_params(self) -> Tuple[dict, dict, dict]:
+        """(tuple) environment variables, additional initialization args, and expected
+        instance attributes for testing initialization from environment variables."""
+        return (
+            {
+                "LLAMA_STACK_API_KEY": "test-key",  # we need this for test_serdes
+            },
+            {
+                "model": "test-a-model",
+            },
+            {
+                # when using LLAMA_STACK_API_KEY env, the key is not stored on
+                # ChatLlamaStack, it is stored in the underlying LlamaStackClient.
+                # therefore, we expect the key to be None on ChatLlamaStack and
+                # cannot reasonably check it.
+                # "api_key": None,
+            },
+        )
 
     # TODO(mf): re-enable when we can extend the test suite
     # import pytest_mock
